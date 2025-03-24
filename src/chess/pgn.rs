f26fea71666d6e07b6130b437d0459f7dd1786d3
Aloïs Rautureau <alois.rautureau@ens-rennes.fr>
2025-03-24 02:51:59 +0100

    Simple PGN structure

--- src/chess/pgn.rs
+++ src/chess/pgn.rs
@@ -1,1 +1,31 @@
 //! # Portable Game Notation standard implementation
+//! The implementation follows the 2023-03-22 revision of the PGN standard which
+//! can be found [here](https://github.com/fsmosca/PGN-Standard/blob/master/PGN-Standard.txt).
+
+use super::action::SanMove;
+
+/// Tags that may appear within a PGN database.
+pub enum PgnTag {
+    Event,
+    Site,
+    Date,
+    Round,
+    White,
+    Black,
+    Result,
+    Other(String),
+}
+
+/// Tag pair representation for contextual game information.
+pub struct TagPair {
+    pub tag: PgnTag,
+    pub value: String,
+}
+
+/// Represents a fully recorded PGN game, with contextual tags, moves played in order,
+/// as well as annotations.
+pub struct PgnGame {
+    tags: Vec<TagPair>,
+    moves: Vec<SanMove>,
+    annotations: Vec<String>,
+}
