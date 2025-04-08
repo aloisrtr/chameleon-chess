7ef668294baa3ff28ebceb2f8b47a23c7170b69a
Aloïs Rautureau <alois.rautureau@ens-rennes.fr>
2025-04-08 16:23:47 +0200

    Better bitboard tests

--- horsey-lib/src/chess/bitboard.rs
+++ horsey-lib/src/chess/bitboard.rs
@@ -1,14 +1,12 @@
 //! # Representation of sets of squares.
-//! Bitboards are an efficient way to represent sets of up to 64 elements,
+//! Bitboards are an efficient way to represent sets of squares,
 //! and are used extensively in the board representation.
 
 use std::iter::FusedIterator;
 
 use super::square::{Delta, Rank, Square};
 
-/// Bitboards are data structures used to efficiently represent the board state.
-///
-/// They are augmented u64 values.
+/// Bitboards are data structures used to efficiently represent a set of squares.
 #[repr(transparent)]
 #[derive(Clone, Copy, PartialEq, Eq, Ord, PartialOrd, Hash, Default)]
 pub struct Bitboard(pub(crate) u64);
@@ -25,179 +23,313 @@ impl Bitboard {
         Self::EMPTY
     }
 
-    /// Returns the universal set (all bits to 1).
+    /// Returns the universal set (contains all squares).
     #[inline]
     pub const fn universe() -> Self {
         Self::UNIVERSE
     }
 
     /// Checks if a bitboard is empty.
+    /// # Example
+    /// ```
+    /// # use horsey::chess::bitboard::*;
+    /// assert!(Bitboard::empty().is_empty());
+    /// ```
     #[inline]
     pub const fn is_empty(&self) -> bool {
         self.0 == 0
     }
 
     /// Checks if a bitboard has one or more bits set.
+    /// # Example
+    /// ```
+    /// # use horsey::chess::bitboard::*;
+    /// assert!(Bitboard::universe().is_not_empty());
+    /// ```
     #[inline]
     pub const fn is_not_empty(&self) -> bool {
         self.0 != 0
     }
 
-    /// Sets the square to occupied.
+    /// Adds a square to the set.
     #[inline]
-    pub const fn set(&mut self, square: Square) {
+    pub const fn insert(&mut self, square: Square) {
         self.0 |= square.bitboard().0
     }
 
-    /// Sets the square to empty.
+    /// Removes a square from the set.
     #[inline]
-    pub const fn unset(&mut self, square: Square) {
+    pub const fn remove(&mut self, square: Square) {
         self.0 &= !square.bitboard().0
     }
 
-    /// Toggles the occupancy of the square (if it was occupied it is now empty
-    /// and vice-versa).
+    /// Toggles a square (if it was present it is removed and vice-versa).
     #[inline]
     pub const fn toggle(&mut self, square: Square) {
         self.0 ^= square.bitboard().0
     }
 
     /// Checks if a given square is set on the bitboard.
+    /// # Example
+    /// ```
+    /// # use horsey::chess::bitboard::*;
+    /// # use horsey::chess::square::*;
+    /// let bb = Bitboard::from_iter([Square::E5, Square::D7]);
+    /// assert!(bb.is_set(Square::E5));
+    /// assert!(!bb.is_set(Square::E6));
+    /// ```
     #[inline]
     pub const fn is_set(&self, square: Square) -> bool {
         self.intersects(square.bitboard())
     }
 
-    /// Checks if a bitboard has only one bit set.
+    /// Checks if a bitboard contains exactly one element.
+    /// # Example
+    /// ```
+    /// # use horsey::chess::bitboard::*;
+    /// # use horsey::chess::square::*;
+    /// let bb = Bitboard::from_iter([Square::E5]);
+    /// assert!(bb.is_single_populated());
+    /// ```
     #[inline]
     pub const fn is_single_populated(&self) -> bool {
         self.0.is_power_of_two()
     }
 
-    /// Checks if a bitboard has zero or one bits set.
+    /// Checks if a bitboard contains zero or one elements.
+    /// # Example
+    /// ```
+    /// # use horsey::chess::bitboard::*;
+    /// # use horsey::chess::square::*;
+    /// let bb = Bitboard::from_iter([Square::E5]);
+    /// assert!(bb.has_at_most_one());
+    /// assert!(Bitboard::empty().has_at_most_one());
+    /// ```
     #[inline]
     pub const fn has_at_most_one(&self) -> bool {
         (self.0 & self.0.wrapping_sub(1)) == 0
     }
 
-    /// Checks if a bitboard has more than one bit set.
+    /// Checks if a bitboard has more than one element.
+    /// # Example
+    /// ```
+    /// # use horsey::chess::bitboard::*;
+    /// # use horsey::chess::square::*;
+    /// let bb = Bitboard::from_iter([Square::E5]);
+    /// assert!(!bb.has_more_than_one());
+    /// let bb = Bitboard::from_iter([Square::E5, Square::D7]);
+    /// assert!(bb.has_more_than_one());
+    /// ```
     #[inline]
     pub const fn has_more_than_one(&self) -> bool {
         (self.0 & self.0.wrapping_sub(1)) != 0
     }
 
     /// Returns the cardinality of the bitboard (i.e. how many squares are set).
+    /// # Example
+    /// ```
+    /// # use horsey::chess::bitboard::*;
+    /// # use horsey::chess::square::*;
+    /// let bb = Bitboard::from_iter([Square::E5, Square::D7]);
+    /// assert_eq!(bb.len(), 2);
+    /// assert_eq!(Bitboard::empty().len(), 0);
+    /// assert_eq!(Bitboard::universe().len(), 64);
+    /// ```
     #[inline]
-    pub const fn cardinality(&self) -> u8 {
+    pub const fn len(&self) -> u8 {
         self.0.count_ones() as u8
     }
 
-    /// Returns the index of the LS1B in the bitboard.
+    /// Returns the lowest square in the bitboard.
     ///
     /// If the bitboard is empty, returns `None`.
+    /// # Example
+    /// ```
+    /// # use horsey::chess::bitboard::*;
+    /// # use horsey::chess::square::*;
+    /// let bb = Bitboard::from_iter([Square::E5, Square::D7]);
+    /// assert_eq!(bb.lowest_square(), Some(Square::E5));
+    /// assert_eq!(Bitboard::empty().lowest_square(), None);
+    /// ```
     #[inline]
-    pub const fn lowest_set_square(&self) -> Option<Square> {
+    pub const fn lowest_square(&self) -> Option<Square> {
         if !self.is_empty() {
-            Some(unsafe { self.lowest_set_square_unchecked() })
+            Some(unsafe { self.lowest_square_unchecked() })
         } else {
             None
         }
     }
 
-    /// Returns the index of the LS1B in the bitboard.
+    /// Returns the lowest square in the bitboard.
     ///
     /// # Safety
-    /// If passed an empty bitboard, the index will be 64 (out of bounds thus not usable).
+    /// If passed an empty bitboard, the square will not contain a correct value (undefined behavior).
     #[inline]
-    pub const unsafe fn lowest_set_square_unchecked(&self) -> Square {
+    pub const unsafe fn lowest_square_unchecked(&self) -> Square {
         unsafe { Square::from_index_unchecked(self.0.trailing_zeros() as u8) }
     }
 
-    /// Pops the LS1B in the bitboard and returns its index.
+    /// Pops the lowest square in the bitboard.
     ///
     /// If the bitboard is empty, returns `None`.
+    /// # Example
+    /// ```
+    /// # use horsey::chess::bitboard::*;
+    /// # use horsey::chess::square::*;
+    /// let mut bb = Bitboard::from_iter([Square::E5, Square::D7]);
+    /// assert_eq!(bb.pop_lowest_square(), Some(Square::E5));
+    /// assert_eq!(bb.pop_lowest_square(), Some(Square::D7));
+    /// assert_eq!(bb.pop_lowest_square(), None);
+    /// ```
     #[inline]
-    pub fn pop_lowest_set_square(&mut self) -> Option<Square> {
+    pub fn pop_lowest_square(&mut self) -> Option<Square> {
         if !Bitboard::is_empty(self) {
-            Some(unsafe { self.pop_lowest_set_square_unchecked() })
+            Some(unsafe { self.pop_lowest_square_unchecked() })
         } else {
             None
         }
     }
 
-    /// Pops the LS1B in the bitboard and returns its index.
+    /// Pops the lowest square in the bitboard.
+    ///
     /// # Safety
-    /// If passed an empty bitboard, the returned index will be 64 (out of bounds thus not usable)
+    /// If passed an empty bitboard, the square will not contain a correct value (undefined behavior).
     #[inline]
-    pub unsafe fn pop_lowest_set_square_unchecked(&mut self) -> Square {
-        let square = unsafe { self.lowest_set_square_unchecked() };
+    pub unsafe fn pop_lowest_square_unchecked(&mut self) -> Square {
+        let square = unsafe { self.lowest_square_unchecked() };
         self.0 &= self.0.wrapping_sub(1);
         square
     }
 
-    /// Returns the index of the MS1B in the bitboard.
+    /// Returns the highest square in the bitboard.
     ///
     /// If the bitboard is empty, returns `None`.
+    /// # Example
+    /// ```
+    /// # use horsey::chess::bitboard::*;
+    /// # use horsey::chess::square::*;
+    /// let bb = Bitboard::from_iter([Square::E5, Square::D7]);
+    /// assert_eq!(bb.highest_square(), Some(Square::D7));
+    /// assert_eq!(Bitboard::empty().highest_square(), None);
+    /// ```
     #[inline]
-    pub const fn highest_set_square(&self) -> Option<Square> {
+    pub const fn highest_square(&self) -> Option<Square> {
         if !self.is_empty() {
-            Some(unsafe { self.highest_set_square_unchecked() })
+            Some(unsafe { self.highest_square_unchecked() })
         } else {
             None
         }
     }
 
-    /// Returns the index of the MS1B in the bitboard.
-    /// # Safety
-    /// If passed an empty bitboard, the index will be 64 (out of bounds thus not usable).
-    #[inline]
-    pub const unsafe fn highest_set_square_unchecked(&self) -> Square {
-        unsafe { Square::from_index_unchecked(self.0.leading_zeros() as u8) }
-    }
-
-    /// Pops the MS1B in the bitboard and returns its index.
-    ///
-    /// If the bitboard is empty, returns `None`.
-    #[inline]
-    pub fn pop_highest_set_square(&mut self) -> Option<Square> {
-        if !Bitboard::is_empty(self) {
-            Some(unsafe { self.pop_highest_set_square_unchecked() })
-        } else {
-            None
-        }
-    }
-
-    /// Pops the LS1B in the bitboard and returns its index.
+    /// Returns the highest square in the bitboard.
     /// # Safety
     /// If passed an empty bitboard, the returned index will be 64 (out of bounds thus not usable)
     #[inline]
-    pub unsafe fn pop_highest_set_square_unchecked(&mut self) -> Square {
-        let square = unsafe { self.highest_set_square_unchecked() };
+    pub const unsafe fn highest_square_unchecked(&self) -> Square {
+        unsafe { Square::from_index_unchecked(63 - self.0.leading_zeros() as u8) }
+    }
+
+    /// Pops the highest square in the bitboard.
+    ///
+    /// If the bitboard is empty, returns `None`.
+    /// # Example
+    /// ```
+    /// # use horsey::chess::bitboard::*;
+    /// # use horsey::chess::square::*;
+    /// let mut bb = Bitboard::from_iter([Square::E5, Square::D7]);
+    /// assert_eq!(bb.pop_highest_square(), Some(Square::D7));
+    /// assert_eq!(bb.pop_highest_square(), Some(Square::E5));
+    /// assert_eq!(bb.pop_highest_square(), None);
+    /// ```
+    #[inline]
+    pub fn pop_highest_square(&mut self) -> Option<Square> {
+        if !Bitboard::is_empty(self) {
+            Some(unsafe { self.pop_highest_square_unchecked() })
+        } else {
+            None
+        }
+    }
+
+    /// Pops the highest square in the bitboard.
+    ///
+    /// # Safety
+    /// If passed an empty bitboard, the returned index will be 64 (out of bounds thus not usable)
+    #[inline]
+    pub unsafe fn pop_highest_square_unchecked(&mut self) -> Square {
+        let square = unsafe { self.highest_square_unchecked() };
         *self ^= square.bitboard();
         square
     }
 
     /// Rotates the bitboard. This can be thought of as "looking at it from the
     /// opponent's perspective".
+    /// # Example
+    /// ```
+    /// # use horsey::chess::bitboard::*;
+    /// # use horsey::chess::square::*;
+    /// let mut bb = Bitboard::from_iter([Square::E5, Square::D7]);
+    /// let rotated_squares = bb.rotate().into_iter().collect::<Vec<_>>();
+    /// assert_eq!(rotated_squares, vec![Square::D2, Square::E4])
+    /// ```
     #[inline]
     pub fn rotate(&self) -> Self {
         Self(self.0.swap_bytes())
     }
 
-    /// Returns true if two bitboards intersect i.e. have at least one common
+    /// Returns `true` if two bitboards intersect i.e. have at least one common
     /// set square.
+    /// # Example
+    /// ```
+    /// # use horsey::chess::bitboard::*;
+    /// # use horsey::chess::square::*;
+    /// let mut bb1 = Bitboard::from_iter([Square::E5, Square::D7]);
+    /// let mut bb2 = Bitboard::from_iter([Square::A4, Square::B6, Square::E5]);
+    /// assert!(bb1.intersects(bb2))
+    /// ```
     #[inline]
     pub const fn intersects(&self, other: Self) -> bool {
         self.0 & other.0 != 0
     }
 
-    /// Returns the intersection of two bitboard.
+    /// Returns the intersection of two bitboards.
+    /// # Example
+    /// ```
+    /// # use horsey::chess::bitboard::*;
+    /// # use horsey::chess::square::*;
+    /// let mut bb1 = Bitboard::from_iter([Square::E5, Square::D7]);
+    /// let mut bb2 = Bitboard::from_iter([Square::A4, Square::B6, Square::E5]);
+    /// let intersection = bb1.intersection(bb2).into_iter().collect::<Vec<_>>();
+    /// assert_eq!(intersection, vec![Square::E5])
+    /// ```
     #[inline]
     pub const fn intersection(&self, other: Self) -> Self {
         Self(self.0 & other.0)
     }
 
-    /// Shifts all bits of the bitboard in the given direction.
+    /// Returns the union of two bitboards.
+    /// # Example
+    /// ```
+    /// # use horsey::chess::bitboard::*;
+    /// # use horsey::chess::square::*;
+    /// let mut bb1 = Bitboard::from_iter([Square::E5, Square::D7]);
+    /// let mut bb2 = Bitboard::from_iter([Square::A4, Square::B6, Square::E5]);
+    /// let union = bb1.union(bb2).into_iter().collect::<Vec<_>>();
+    /// assert_eq!(union, vec![Square::A4, Square::E5, Square::B6, Square::D7])
+    /// ```
+    #[inline]
+    pub const fn union(&self, other: Self) -> Self {
+        Self(self.0 | other.0)
+    }
+
+    /// Shifts all squares of the bitboard in the given direction.
+    /// # Example
+    /// ```
+    /// # use horsey::chess::bitboard::*;
+    /// # use horsey::chess::square::*;
+    /// let mut bb = Bitboard::from_iter([Square::E5, Square::D7]);
+    /// let shifted_north = bb.shift(Delta::North).into_iter().collect::<Vec<_>>();
+    /// assert_eq!(shifted_north, vec![Square::E6, Square::D8])
+    /// ```
     #[inline]
     pub const fn shift(&self, delta: Delta) -> Self {
         Self(if 0 < delta as i8 {
@@ -207,13 +339,19 @@ impl Bitboard {
         })
     }
 
-    /// Inverts the bitboard (set bits become unset and vice-versa).
+    /// Inverts the bitboard (non-members of the set become members and vice-versa).
+    /// # Example
+    /// ```
+    /// # use horsey::chess::bitboard::*;
+    /// assert_eq!(Bitboard::universe().invert(), Bitboard::empty())
+    /// ```
     #[inline]
     pub const fn invert(&self) -> Self {
         Self(!self.0)
     }
 
     /// Architecture independant PEXT (Parallel Bits Extract) implementation.
+    ///
     /// Will be slower without the `bmi2` CPU flag.
     #[inline(always)]
     pub fn pext(self, mask: Self) -> Self {
@@ -237,7 +375,9 @@ impl Bitboard {
     }
 
     /// Architecture independant PDEP (Parallel Bits Deposit) implementation.
+    ///
     /// Will be slower without the `bmi2` CPU flag.
+    #[inline(always)]
     pub fn pdep(self, mask: Self) -> Self {
         #[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
         {
@@ -277,6 +417,8 @@ impl Bitboard {
     }
 
     /// Returns this bitboard as an array of bytes.
+    ///
+    /// Mainly used for FEN compression.
     #[inline]
     pub const fn as_bytes(self) -> [u8; 8] {
         self.0.to_be_bytes()
@@ -542,12 +684,12 @@ impl Iterator for Bitboard {
 
     #[inline]
     fn next(&mut self) -> Option<Self::Item> {
-        self.pop_lowest_set_square()
+        self.pop_lowest_square()
     }
 
     #[inline]
     fn size_hint(&self) -> (usize, Option<usize>) {
-        let size = self.cardinality();
+        let size = self.len();
         (size as usize, Some(size as usize))
     }
 }
@@ -555,21 +697,21 @@ impl Iterator for Bitboard {
 impl DoubleEndedIterator for Bitboard {
     #[inline]
     fn next_back(&mut self) -> Option<Self::Item> {
-        self.pop_highest_set_square()
+        self.pop_highest_square()
     }
 }
 impl FusedIterator for Bitboard {}
 impl ExactSizeIterator for Bitboard {
     #[inline]
     fn len(&self) -> usize {
-        self.cardinality() as usize
+        self.len() as usize
     }
 }
 impl FromIterator<Square> for Bitboard {
     fn from_iter<T: IntoIterator<Item = Square>>(iter: T) -> Self {
         let mut bb = Self::default();
         for sq in iter {
-            bb.set(sq);
+            bb.insert(sq);
         }
         bb
     }
@@ -577,7 +719,7 @@ impl FromIterator<Square> for Bitboard {
 impl Extend<Square> for Bitboard {
     fn extend<T: IntoIterator<Item = Square>>(&mut self, iter: T) {
         for sq in iter {
-            self.set(sq);
+            self.insert(sq);
         }
     }
 }
